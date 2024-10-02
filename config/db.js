7d2d972dad00647403c1b4204d89d59d6df683ac
Alperen Erdem <alpernerdm@gmail.com>
2024-10-02 20:01:51 +0300

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+const db= ()=>{
+    mongoose.connect(process.env.DATABASE_CONNECTION)
+    .then(()=>{
+        console.log("Veritabanına bağlandı");
+    })
+        .catch((err)=>{
+        console.log("Veritabanına bağlanamadı"+err)
+        })
+}
+
+module.exports=db;

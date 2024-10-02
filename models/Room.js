a948384922efc9bcab8733b13b6deb9876685fdf
Alperen Erdem <alpernerdm@gmail.com>
2024-10-02 20:28:12 +0300

    created models

---
+++ models/Room.js
@@ -0,0 +1,29 @@
+const { timeStamp } = require('console')
+const mongoose=require('mongoose')
+
+const roomSchema=mongoose.Schema({
+    type:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    desc:{
+        type:String,
+        required:true
+    },
+    maxPeople:{
+        type:Number,
+        required:true
+    },
+    roomNumbers:[{
+        number:Number,
+        unvailableDates:{
+            type:[Date],
+        }
+    }]
+},{timeStamp:true})
+
+module.exports=mongoose.model('Room',roomSchema)
